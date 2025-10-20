012215efc6b34829177377273ec5a5b6982327e5
carlosalbertofilho <carlosalberto_filho@outlook.com>
2025-10-20 14:42:39 -0300

    Update build configuration: enable builder and adjust image parent; refine package list in build script

--- images/nanoemacs/build.sh
+++ images/nanoemacs/build.sh
@@ -8,7 +8,7 @@
 #     kubler.sh build -i emacs/nanoemacs
 # ..and then search the Portage database:
 #     eix <search-string>
-_packages="app-editors/emacs dev-vcs/git dev-util/github-cli sys-apps/ripgrep sys-apps/fd app-shells/fzf sys-devel/gcc dev-util/cmake sys-devel/gdb sys-devel/clang dev-util/valgrind net-misc/curl app-text/pandoc dev-lang/python dev-util/pkgconf app-text/ispell media-libs/fontconfig sys-process/procps app-arch/unzip net-misc/wget dev-util/clang-common media-libs/libvips app-text/poppler media-video/ffmpegthumbnailer media-libs/mediainfo app-arch/p7zip sys-apps/coreutils app-text/epub-thumbnailer media-gfx/imagemagick sys-apps/file app-shells/bash net-misc/rsync"
+_packages="app-editors/emacs dev-vcs/git dev-util/github-cli sys-apps/ripgrep sys-apps/fd app-shells/fzf sys-devel/gcc dev-build/cmake dev-debug/gdb llvm-core/clang dev-debug/valgrind net-misc/curl app-text/pandoc-bin dev-lang/python dev-util/pkgconf app-text/hunspell media-libs/fontconfig sys-process/procps app-arch/unzip net-misc/wget llvm-core/clang-common media-libs/vips app-text/poppler media-video/ffmpegthumbnailer media-video/mediainfo app-arch/p7zip sys-apps/coreutils media-gfx/imagemagick sys-apps/file app-shells/bash net-misc/rsync"
 # Install a standard system directory layout at ${_EMERGE_ROOT}, optional, default: false
 #BOB_INSTALL_BASELAYOUT=true
 
@@ -83,7 +83,6 @@ configure_rootfs_build()
     update_keywords 'sys-apps/fd' '+~amd64'
     update_keywords 'media-libs/libvips' '+~amd64'
     update_keywords 'media-video/ffmpegthumbnailer' '+~amd64'
-    update_keywords 'app-text/epub-thumbnailer' '+~amd64'
     
 }
 
